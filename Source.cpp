25c55600d90600c790badcabccd586c6ad2fa64a
Matthew Apelman <31864387+Iplaygames995@users.noreply.github.com>
2018-02-28 14:30:04 -0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,28 +1,34 @@
+#include <allegro5\allegro.h>
+#include <allegro5/allegro_primitives.h>
 
-#include<allegro5\allegro.h>
-#include<allegro5\allegro_primitives.h>
-#include<iostream>
-#include<ctime>
+int main() {
+	ALLEGRO_DISPLAY *Win = NULL;
 
-using namespace std;
-
-int main()
-{
-	ALLEGRO_DISPLAY *window = NULL;
-	
 	al_init();
 	al_init_primitives_addon();
-	window = al_create_display(800,800);
-	srand(time(NULL));
-	while (1) {
-		al_draw_filled_circle(rand()%800, rand()%800, rand() % 80, al_map_rgb(rand() % 255, rand() % 225, rand() % 255));
-		
-		//al_draw_filled_triangle(rand() % 800, rand() % 800, rand() % 80 al_map_rgb((rand() % 255, rand() % 225, rand() % 800, rand() % 800, rand() % 80, al_map_rgb(rand() % 255, rand() % 225, rand() % 255)) rand( % 255));
-		al_flip_display();
-		//al_rest(.08);
 
-	}
-	al_destroy_display(window);
+	Win = al_create_display(600, 600);
 
+	al_clear_to_color(al_map_rgb(255, 255, 255));
+
+	//al_draw_filled_rectangle(300, 200, 350, 20, al_map_rgb(60, 595, 500));
+
+	al_draw_filled_rectangle(500, 550, 400, 600, al_map_rgb(0, 0, 255));
+	al_draw_filled_rectangle(200, 400, 400, 600, al_map_rgb(255, 0, 0));
+	al_draw_filled_triangle(300, 250, 200, 400, 400, 400, al_map_rgb(0, 0, 0));
+
+	al_draw_filled_circle(500, 100, 75, al_map_rgb(255, 255, 0));
+
+	al_draw_filled_circle(100, 100, 75, al_map_rgb(128, 128, 128));
+	al_draw_filled_circle(150, 100, 75, al_map_rgb(128, 128, 128));
+	al_draw_filled_circle(200, 100, 75, al_map_rgb(128, 128, 128));
+
+	al_flip_display();
+
+	al_rest(20);
+
+	al_destroy_display(Win);
 }
+
+
 
