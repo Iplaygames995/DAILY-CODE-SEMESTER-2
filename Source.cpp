b470b67159039ed442d74d3b3ddcc2faac6f7b7f
Matthew Apelman <31864387+Iplaygames995@users.noreply.github.com>
2018-03-21 14:09:34 -0600

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,27 +1,76 @@
-#include <iostream>
-#include <vector>
-#include<algorithm>
+#include<vector>
+#include<string>
+#include<iostream>
 
 using namespace std;
+class regalia {
+private:
+	int  fuel;
+	string brand;
+	string color;
+	bool drive;
 
-int main() {
-	int input = 5;
-	int temp;
-	vector<int> one;
-	vector<int> ::iterator i;
+public:
 
-	while (input != 0) {
-		cin >> input;
-		one.push_back(input);
+	void initcars(int f, string b, string c, bool d);
+	void printinfo();
 
+ };
 
-	}
-	sort(one.begin(), one.end());
+	
 
-	for (i = one.begin(); i < one.end(); i++) {
-		cout << "the numbers in order are: ";
-		cout << *i << endl;
+int main()
 
-	}
+{
+	vector <regalia> garage;
+	vector <regalia>::iterator iter;
+
+	regalia dodge;
+	regalia audi;
+	regalia bugati;
+	regalia harley;
+	regalia ford;
+
+	audi.initcars(100, "audi", "blue", true);
+
+	ford.initcars(120, "ford", "yellow", false);
+
+	harley.initcars(140, "regallia", "black", true);
+
+	dodge.initcars(160, "dodge", "red", false);
+
+	bugati.initcars(180, "bugati", "orange", false);
+
+	garage.push_back(bugati);
+
+	garage.push_back(audi);
+
+	garage.push_back(harley);
+
+	garage.push_back(ford);
+
+	garage.push_back(dodge);
+
+	for (iter = garage.begin(); iter != garage.end(); iter++)
+		iter->printinfo();
 	system("pause");
+}
+
+
+
+	
+void regalia::initcars(int f, string b, string c, bool d) {
+	fuel = f;
+	brand = b;
+	color = c;
+	drive = d;
+}
+void regalia::printinfo(){
+	cout << "your car brand is " << brand << endl;
+	cout << "your fuel is at " << fuel << endl;
+	cout << "your color is  " << color << endl;
+	if (drive != true)
+		cout << "your car is not four wheel drive" << endl;
+	else;
+	cout << "your car is four wheel drive" << endl;
 }
