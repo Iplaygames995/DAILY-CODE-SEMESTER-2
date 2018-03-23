85692cfe7320636441bf89d392cd7d5e8e70c814
Matthew Apelman <31864387+Iplaygames995@users.noreply.github.com>
2018-03-23 13:09:32 -0600

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,76 +1,48 @@
-#include<vector>
 #include<string>
 #include<iostream>
+#include<allegro5\allegro.h>
+#include<allegro5\allegro_primitives.h>
+
 
 using namespace std;
-class regalia {
-private:
-	int  fuel;
-	string brand;
-	string color;
-	bool drive;
+class face {
+
+private:	
+	int xpos;
+	int ypos;
+	int xvelocity;
+	int yvelocity;
+
+	ALLEGRO_BITMAP*win;
+
+
 
 public:
+	void initclass(int xp, int yp, int xv, int yv,ALLEGRO_BITMAP*window);
+	void draw();
+	
 
-	void initcars(int f, string b, string c, bool d);
-	void printinfo();
+};
 
- };
+int main(){
+
+	while (1);
+	face b1;
+	b1.initclass(200,200);
+		
+}
+void face::initclass(int xp, int yp, int xv, int yv, ALLEGRO_BITMAP*window) {
+	xpos = xp;
+	ypos = yp;
+	xvelocity = xv;
+	yvelocity = yv;
+	win = window;
 
 	
 
-int main()
-
-{
-	vector <regalia> garage;
-	vector <regalia>::iterator iter;
-
-	regalia dodge;
-	regalia audi;
-	regalia bugati;
-	regalia harley;
-	regalia ford;
-
-	audi.initcars(100, "audi", "blue", true);
-
-	ford.initcars(120, "ford", "yellow", false);
-
-	harley.initcars(140, "regallia", "black", true);
-
-	dodge.initcars(160, "dodge", "red", false);
-
-	bugati.initcars(180, "bugati", "orange", false);
-
-	garage.push_back(bugati);
-
-	garage.push_back(audi);
-
-	garage.push_back(harley);
-
-	garage.push_back(ford);
-
-	garage.push_back(dodge);
-
-	for (iter = garage.begin(); iter != garage.end(); iter++)
-		iter->printinfo();
-	system("pause");
 }
+void face::draw() {
 
 
 
-	
-void regalia::initcars(int f, string b, string c, bool d) {
-	fuel = f;
-	brand = b;
-	color = c;
-	drive = d;
-}
-void regalia::printinfo(){
-	cout << "your car brand is " << brand << endl;
-	cout << "your fuel is at " << fuel << endl;
-	cout << "your color is  " << color << endl;
-	if (drive != true)
-		cout << "your car is not four wheel drive" << endl;
-	else;
-	cout << "your car is four wheel drive" << endl;
 }
